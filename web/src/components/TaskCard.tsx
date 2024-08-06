f52e93200690a6dfcd2be6bbcbb4c9c55602a0cd
Swati Shivam Upadhyay <mujhkorokegakaun@gmail.com>
2024-08-06 06:06:13 +0530

    Update TaskCard.tsx

--- web/src/components/TaskCard.tsx
+++ web/src/components/TaskCard.tsx
@@ -16,10 +16,10 @@ const TaskCard: React.FC<TaskCardProps> = ({ task, onComplete, onInProgress }) =
         <Typography variant="h5">{task.title}</Typography>
         <Typography variant="body2">{task.description}</Typography>
         <div>
-          {task.status === 'todo' && (
+          {  (
             <Button variant="outlined" onClick={() => onInProgress(task)}>Start</Button>
           )}
-          {task.status === 'inprogress' && (
+          { (
             <Button variant="outlined" color="success" onClick={() => onComplete(task)}>Complete</Button>
           )}
         </div>
